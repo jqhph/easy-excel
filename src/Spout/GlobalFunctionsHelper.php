cc805a2a8f811b0ee6f680954ce78ebfdc13f43b
jqh <841324345@qq.com>
2020-05-24 13:18:57 +0800

    注释

--- src/Spout/GlobalFunctionsHelper.php
+++ src/Spout/GlobalFunctionsHelper.php
@@ -6,14 +6,6 @@ use Box\Spout\Common\Helper\GlobalFunctionsHelper as Helper;
 
 class GlobalFunctionsHelper extends Helper
 {
-    /**
-     * Wrapper around global function basename()
-     * @see basename()
-     *
-     * @param string $path
-     * @param string|null $suffix
-     * @return string
-     */
     public function basename($path, $suffix = null)
     {
         $path = str_replace('\\', '/', $path);
